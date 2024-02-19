5d299068671fc488d894e915c52d2e8de5d0540b
Ferdaussk <mdjannatferdaus82247@gmail.com>
2024-02-19 17:49:29 +0600

    4.1.1v

--- includes/dashboard.php
+++ includes/dashboard.php
@@ -1137,6 +1137,13 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 		'bwdeb-plugin-main-menu',
 		'bwdeb-plugin-switch-section'
 	);
+	add_settings_field(
+		'bwdeb-notation',
+		'',
+		'bwdeb_plugin_function_for_datas_callback',
+		'bwdeb-plugin-main-menu',
+		'bwdeb-plugin-switch-section'
+	);
   
   // Registered the add switch control here
 	register_setting( 
@@ -1783,4 +1790,8 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 	register_setting( 
 		'bwdeb-plugin-settings', 
 		'bwdeb-3d-text' 
+	);
+	register_setting( 
+		'bwdeb-plugin-settings', 
+		'bwdeb-notation' 
 	);
